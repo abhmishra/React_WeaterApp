6fce7de034e478bf04b5ad9ae17286b87cdcb485
abhmishra <52232890+abhmishra@users.noreply.github.com>
2024-10-14 08:58:21 +0530

    Update apiKey.js

--- src/apiKey.js
+++ src/apiKey.js
@@ -1,1 +1,2 @@
-    export const API_KEY = "5fd397b40ba18a510fbb2bed3b488d37"
+    export const API_KEY = "Enter your API Key here"
+// "5fd397b40ba18a510fbb2bed3b488d37" - This one is demo key and has only 1000 response per day 
